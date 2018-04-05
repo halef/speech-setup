af34119334aceccacd89bd2f777ccd163adc3973
langep <patrick.l.lange@gmail.com>
2018-04-04 17:47:21 -0700

    Initial commit

---
+++ install_dependencies.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+set -e # Abort on error
+
+# Locate this script.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Set paramaters
+# TODO(langep): Make parameters configurable
+download_location=~/src
+install_location=/opt/tools
+gradle_version=3.4.1
+maven_version=3.3.9
+
+jdk_archive=jdk-8u161-linux-x64.tar.gz
+jdk_url=http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/${jdk_archive}
+
+gradle_archive=gradle-${gradle_version}-bin.zip
+maven_archive=apache-maven-${maven_version}-bin.tar.gz
+gradle_url=https://downloads.gradle.org/distributions/${gradle_archive}
+maven_url=http://www-eu.apache.org/dist/maven/maven-3/${maven_version}/binaries/${maven_archive}
+
+maven_name=${maven_archive%-bin.tar.gz}
+gradle_name=${gradle_archive%-bin.zip}
+jdk_name=jdk1.8.0_161
+
+# Cleanup trap in case of error
+cleanup() {
+    if [ $? -ne 0 ]; then
+        # TODO(langep): Conditional cleanup based on where error happend
+        rm -rf "$install_location"
+    fi
+}
+
+trap cleanup EXIT
+
+# Update packages and install dependencies
+apt-get update
+apt-get install -y --no-install-recommends build-essential unzip git wget \
+    lib32z1 sharutils cmake zlib1g-dev libssl-dev g++ automake autoconf \
+    libtool subversion python-minimal libatlas3-base
+
+# Make download and install directories
+mkdir -p "$download_location" "$install_location"
+cd "${download_location}"
+
+# Download and unpack the source archive
+if [ ! -f ${gradle_archive} ]; then
+    wget -O $gradle_archive $gradle_url
+fi
+
+if [ ! -f ${maven_archive} ]; then
+    wget -O $maven_archive $maven_url
+fi
+
+if [ ! -f ${jdk_archive} ]; then
+    wget --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+        --no-check-certificate -c -O $jdk_archive $jdk_url
+fi
+
+# Unpacking and installing
+
+tar -xvf $maven_archive
+tar -xvf $jdk_archive
+unzip $gradle_archive
+
+cp -r $maven_name "${install_location}/${maven_name}"
+ln -s "${install_location}/${maven_name}" "${install_location}/maven"
+
+cp -r $gradle_name "${install_location}/${gradle_name}"
+ln -s ${install_location}/${gradle_name} "${install_location}/gradle"
+
+cp -r $jdk_name "${install_location}/${jdk_name}"
+ln -s ${install_location}/${jdk_name} "${install_location}/java"
+
+info "Press [Q] to install JSAPI"
+sh ${SCRIPT_DIR}/jsapi.sh
+mkdir -p ${install_location}/java/lib/ext
+mv jsapi.jar ${install_location}/java/lib/ext/jsapi-1.0.jar
+
+# Finishing setup of environment
+echo "export JAVA_HOME=${install_location}/java" >> /etc/bash.bashrc
+echo "export GRADLE_HOME=${install_location}/gradle" >> /etc/bash.bashrc
+echo "export M2_HOME=${install_location}/maven" >> /etc/bash.bashrc
+echo 'export PATH=${M2_HOME}/bin:${GRADLE_HOME}/bin:${JAVA_HOME}/bin:${PATH}' >> /etc/bash.bashrc
---
+++ install_marytts.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+set -e # Abort on error
+
+# Locate this script.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Set paramaters
+# TODO(langep): Make parameters configurable
+download_location=~/src
+install_location=/opt/marytts
+repo_url=https://github.com/marytts/marytts.git
+branch=master
+# Voices
+poppy_archive=voice-dfki-poppy-5.2.zip
+poppy_url=https://github.com/marytts/voice-dfki-poppy/releases/download/v5.2/${poppy_archive}
+spike_archive=voice-dfki-spike-5.2.zip
+spike_url=https://github.com/marytts/voice-dfki-spike/releases/download/v5.2/${spike_archive}
+
+# Cleanup trap in case of error
+cleanup() {
+    if [ $? -ne 0 ]; then
+        # TODO(langep): Conditional cleanup based on where error happend
+        rm -rf "$install_location"
+    fi
+}
+
+trap cleanup EXIT
+
+# Load heler.sh functions
+source ${SCRIPT_DIR}/helper.sh
+
+# Check dependencies
+require_root
+require_var ${JAVA_HOME:-""} "JAVA_HOME"
+require_var ${GRADLE_HOME:-""} "GRADLE_HOME"
+require_command wget
+require_command git
+require_command unzip
+
+# Make download and install directories
+mkdir -p "$download_location" "$install_location"
+cd "$download_location"
+
+# Clone repo
+if [ -d check_dir marytts ]; then
+    git clone $repo_url marytts
+fi
+pushd marytts
+git checkout $branch
+git pull
+
+# Install
+./gradlew installDist
+cp -r build/install/marytts/* ${install_location}/.
+popd
+
+# Download and install voices
+wget -O $poppy_archive $poppy_url
+wget -O $spike_archive $spike_url
+unzip -o $poppy_archive
+unzip -o $spike_archive
+
+mkdir -p ${install_location}/lib/voices
+cp lib/*.jar  ${install_location}/lib/.
+cp -r lib/voices/* ${install_location}/lib/voices/.
+
+# Create group and user for marytts if they don't exist
+if ! check_group marytts; then
+    groupadd marytts
+fi
+
+if ! check_user marytts; then
+    useradd -r -s /bin/false -g marytts marytts
+fi
+# TODO(langep): Maybe we should verify that marytts user is in marytts group
+
+# Set permissions
+chown -R marytts $install_location
+
+# Setup service
+cp ${SCRIPT_DIR}/init.d/marytts.init-debian /etc/init.d/marytts
+chmod +x /etc/init.d/marytts
+sed -i -e "s|%%MARYTTS_HOME%%|${install_location}|g" /etc/init.d/marytts
+sed -i -e "s|%%JAVA_HOME%%|${JAVA_HOME}|g" /etc/init.d/marytts
+update-rc.d marytts defaults
+
+# Set environment
+echo "export MARYTTS_HOME=${marytts_dir}" >> /etc/bash.bashrc
+
+info "Install complete. Run 'sudo service marytts start'"
